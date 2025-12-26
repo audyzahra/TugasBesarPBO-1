681001eba3358ca6c884ef35e28c2fc954e1ef44
audyzahra <audyzahra21@gmail.com>
2025-12-27 01:15:42 +0700

    Menu Login dan Logout menggunakan 2 tier

--- TugasBesarPBO/src/App.java
+++ TugasBesarPBO/src/App.java
@@ -1,16 +1,23 @@
-import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.UIManager;
 import javax.swing.SwingUtilities;
-import view.MainFrame;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import view.LoginFrame;
 
 public class App {
+
     public static void main(String[] args) {
+
+        // Set Look and Feel
         try {
             UIManager.setLookAndFeel(new FlatLightLaf());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        SwingUtilities.invokeLater(MainFrame::new);
+        // Jalankan aplikasi dari LOGIN (2-TIER)
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 }
